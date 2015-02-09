a14e445d101357f411906d7a87cfdfd4a3369e33
jdude0822 <jnordlan@purdue.edu>
2015-02-09 15:08:25 -0500

    Persistent Login

--- Not Very Good Prototype(Unfinished)/StudyBuddy/app/src/main/java/com/cs408/studybuddy/StudyBuddyLoginActivity.java
+++ Not Very Good Prototype(Unfinished)/StudyBuddy/app/src/main/java/com/cs408/studybuddy/StudyBuddyLoginActivity.java
@@ -3,7 +3,9 @@ package com.cs408.studybuddy;
 import android.app.Activity;
 import android.content.Intent;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.parse.Parse;
@@ -25,6 +27,10 @@ public class StudyBuddyLoginActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean LoggedIn = prefs.getBoolean("Islogin", false);
+        Log.v("login","Boolean value: " + LoggedIn);
+
         // Required - Initialize the Parse SDK
         Parse.initialize(this, getString(R.string.parse_app_id),
                 getString(R.string.parse_client_key));
@@ -43,8 +49,20 @@ public class StudyBuddyLoginActivity extends Activity{
             }
         });
 
-        ParseLoginBuilder builder = new ParseLoginBuilder(StudyBuddyLoginActivity.this);
-        startActivityForResult(builder.build(), LOGIN_REQUEST);
+        if(LoggedIn)           //if the user has already logged in, take them straight to the main page skipping the login screen.
+        {
+            Log.v("login","Skipping Parse");
+            Intent i;
+            i = new Intent(StudyBuddyLoginActivity.this, DrawerActivity.class);
+            startActivity(i);
+            finish();
+        }
+
+        else
+        {
+            ParseLoginBuilder builder = new ParseLoginBuilder(StudyBuddyLoginActivity.this);
+            startActivityForResult(builder.build(), LOGIN_REQUEST);
+        }
     }
 
     @Override
