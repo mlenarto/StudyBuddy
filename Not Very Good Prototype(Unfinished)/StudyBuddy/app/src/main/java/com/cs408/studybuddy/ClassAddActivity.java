344f2355415f0c7664c759297c0c1c93e2bf3ab3
tragessere <tragessere@gmail.com>
2015-02-11 00:14:22 -0500

    Reformatted the ClassAddActivity. Added functionality for locally adding and removing classes.
    Classes are currently added through an EditText which will be swapped for Spinners later

--- Not Very Good Prototype(Unfinished)/StudyBuddy/app/src/main/java/com/cs408/studybuddy/ClassAddActivity.java
+++ Not Very Good Prototype(Unfinished)/StudyBuddy/app/src/main/java/com/cs408/studybuddy/ClassAddActivity.java
@@ -1,26 +1,26 @@
 package com.cs408.studybuddy;
 
+import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -32,41 +32,47 @@ public class ClassAddActivity extends ActionBarActivity
     public EditText newClass;
     public ListView classList;
     private Toolbar mToolbar;
-    public SharedPreferences pref;
-    ArrayAdapter<String> arrayAdapter;
-    ArrayList<String> classes;
+    public SharedPreferences prefs;
+    classAdapter arrayAdapter;
+    List<String> classes;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_add);
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+		classList = (ListView) findViewById(R.id.classList);
+		newClass = (EditText) findViewById(R.id.newClass);
+
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        classes = new ArrayList<>();
+		prefs = getSharedPreferences(getResources().getString(R.string.app_preferences), 0);
 
-        classList = (ListView) findViewById(R.id.classList);
-        newClass = (EditText) findViewById(R.id.newClass);
+		String classString = prefs.getString("class_list", null);
+		if(classString != null && !classString.isEmpty()) {
+			classes = new ArrayList<>(Arrays.asList(classString.split(",")));
+			Collections.sort(classes);
+		} else {
+			classes = new ArrayList<>();
+		}
 
-//        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, classes);
-//        classList.setAdapter(arrayAdapter);
+		arrayAdapter = new classAdapter(this, R.layout.fragment_class_add_item, classes);
+		classList.setAdapter(arrayAdapter);
 
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+			@Override
+			public void onClick(View v) {
+				onBackPressed();
+			}
+		});
 
         setupConfirmButton();
     }
 
     public void setupConfirmButton()
     {
-
-        pref = getSharedPreferences(getResources().getString(R.string.app_preferences), 0);
         Button confirm = (Button) findViewById(R.id.confirmClasses);
 
         confirm.setOnClickListener(new View.OnClickListener()
@@ -76,24 +82,25 @@ public class ClassAddActivity extends ActionBarActivity
                 public void onClick(View arg0)
                 {
                     //TODO: Checks for phony classes
-                    String insert;
-                    insert = newClass.getText().toString();
+                    String insert = newClass.getText().toString();
 
-//                    arrayAdapter.add(insert);
-//                    classes.add(insert);
-//                    pref.edit().putString("classes", convertToString(classes)).commit();
-//                    arrayAdapter.notifyDataSetChanged();
-					String classList = pref.getString("class_list", null);
-					SharedPreferences.Editor edit = pref.edit();
+					String classString = prefs.getString("class_list", null);
+					SharedPreferences.Editor edit = prefs.edit();
 
-					if(classList != null && !classList.isEmpty()) {
-						classList += "," + insert;
-						edit.putString("class_list", classList);
+					if(classString != null && !classString.isEmpty()) {
+						classString += "," + insert;
+						edit.putString("class_list", classString);
 					} else {
 						edit.putString("class_list", insert);
 					}
 					edit.commit();
 
+
+					classes.add(insert);
+					Collections.sort(classes);
+					arrayAdapter.notifyDataSetChanged();
+
+					newClass.setText("");
 
 					//TODO: Add this class to the server as well
                 }
@@ -121,4 +128,88 @@ public class ClassAddActivity extends ActionBarActivity
         ArrayList<String> list = new ArrayList<String>(Arrays.asList(string.split(",")));
         return list;
     }
+
+	private class classAdapter extends ArrayAdapter<String> {
+
+		int resource;
+
+		public classAdapter(Context context, int resource, List<String> objects) {
+			super(context, resource, objects);
+
+			this.resource = resource;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = convertView;
+			TextView classNameText;
+			Button remove;
+			final String className;
+
+			if(v == null) {
+				LayoutInflater li = LayoutInflater.from(getContext());
+				v = li.inflate(resource, parent, false);
+			}
+
+
+			classNameText = (TextView)v.findViewById(R.id.class_name);
+			remove = (Button)v.findViewById(R.id.remove_class);
+
+			className = this.getItem(position);
+			classNameText.setText(className);
+
+
+			remove.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+					builder.setMessage("Are you sure you want to leave this class?")
+							.setTitle("Leave " + className + "?");
+
+					builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							String classesString = prefs.getString("class_list", null);
+							SharedPreferences.Editor edit = prefs.edit();
+
+							int start = classesString.indexOf(className);
+
+							if(classes.size() == 1) {
+								edit.putString("class_list", "");
+							} else if(start == 0) {		//Name is at the start of the array
+								classesString = classesString.substring(className.length()+1, classesString.length());
+								edit.putString("class_list", classesString);
+							} else if((start + className.length()) == classesString.length()) {
+								classesString = classesString.substring(0, start-1);
+								edit.putString("class_list", classesString);
+							} else {
+								classesString = classesString.substring(0, start-1) +
+										classesString.substring(start + className.length(), classesString.length());
+								edit.putString("class_list", classesString);
+							}
+							edit.commit();
+
+							classes.remove(className);
+							Collections.sort(classes);
+							arrayAdapter.notifyDataSetChanged();
+
+							//TODO: remove class on server side
+						}
+					});
+
+					builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							//Do nothing
+						}
+					});
+
+					builder.create().show();
+				}
+			});
+
+			return v;
+		}
+	}
 }
