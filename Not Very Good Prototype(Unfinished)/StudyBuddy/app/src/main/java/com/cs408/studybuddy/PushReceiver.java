b1201d43758a12185dd86c2b47d89e2f4f8fc5aa
Shmoozen <tgrikas@purdue.edu>
2015-03-12 18:54:19 -0400

    Push notifications bypass Splash Screen
    Now, we need to make it go to the request itself. This will require
    editing the cloudcode as well.

--- Not Very Good Prototype(Unfinished)/StudyBuddy/app/src/main/java/com/cs408/studybuddy/PushReceiver.java
+++ Not Very Good Prototype(Unfinished)/StudyBuddy/app/src/main/java/com/cs408/studybuddy/PushReceiver.java
@@ -1,9 +1,13 @@
 package com.cs408.studybuddy;
 
+import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
 import com.parse.ParsePushBroadcastReceiver;
+
+import java.util.List;
 
 /**
  * Created by Tyler on 3/12/2015.
@@ -11,9 +15,13 @@ import com.parse.ParsePushBroadcastReceiver;
 public class PushReceiver extends ParsePushBroadcastReceiver {
     @Override
     public void onPushOpen(Context context, Intent intent) {
-        Intent i = new Intent(context, SplashScreenActivity.class);
+        //TODO: Instead of opening login activity, have it open the request
+        Intent i = new Intent(context, StudyBuddyLoginActivity.class);
         i.putExtras(intent.getExtras());
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(i);
     }
+
+
+
 }
