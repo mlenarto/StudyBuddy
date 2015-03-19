b8ca794dd900d7305e27c8bb169d0b5effc06221
jdude0822 <jnordlan@purdue.edu>
2015-03-19 01:03:29 -0400

    This is silly...

--- Not Very Good Prototype(Unfinished)/StudyBuddy/app/src/main/java/com/cs408/studybuddy/NewRequestActivity.java
+++ Not Very Good Prototype(Unfinished)/StudyBuddy/app/src/main/java/com/cs408/studybuddy/NewRequestActivity.java
@@ -243,7 +243,6 @@ public class NewRequestActivity extends ActionBarActivity {
                         }
                     }
                 });
-                progress.dismiss();
 			}
 		});
 
